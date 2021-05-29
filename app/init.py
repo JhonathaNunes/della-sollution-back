015cc75e2f867932cfcf2ad10b72db374dcdf348
JhonathaNunes <jhonathasilveira@gmail.com>
2021-05-28 21:43:50 -0300

    Fix imports

--- app/init.py
+++ app/init.py
@@ -1,13 +1,16 @@
 from flask import Flask
 
 from .models import db
-import config
+from .config import (
+    SECRET_KEY,
+    DATABASE_CONNECTION_URI
+)
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = config.SECRET_KEY
-    app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_CONNECTION_URI
+    app.config['SECRET_KEY'] = SECRET_KEY
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.app_context().push()
     db.init_app(app)
